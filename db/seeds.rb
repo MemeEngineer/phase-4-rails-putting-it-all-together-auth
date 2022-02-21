f247bb764006cee2d69a3f382418fddab6593283
Johnny Wu <johnnyw131@gmail.com>
2022-02-21 18:04:53 -0500

    commit tree 9bbae7ad9ff10c13a2cc0fa282aba1e304c75f24

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "king", password_digest:"123", image_url: "https://en.wikipedia.org/wiki/SpongeBob_SquarePants_(character)#/media/File:SpongeBob_SquarePants_character.svg", bio:"bye")
